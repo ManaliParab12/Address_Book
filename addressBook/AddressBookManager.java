d56e0201085db147e7af20b28ad9bf001821cd63
ManaliParab12 <manaliparab10@gmail.com>
2020-10-28 15:36:14 +0530

    Created Address Book Manager class with the Structure of all AddressBookManagerInterface Method

---
+++ addressBook/AddressBookManager.java
@@ -0,0 +1,30 @@
+package addressBook;
+
+public class AddressBookManager implements AddressBookManagerInterface{
+
+	
+	public void newAddressBook() {
+		
+	}
+
+
+	public void openAddressBook() {
+		
+	}
+
+	
+	public void saveAddressBook() {
+		
+	}
+
+
+	public void saveAsAddressBook() {
+		
+	}
+
+	
+	public void closeAddressBook() {
+		
+	}
+
+}
