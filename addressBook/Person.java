7d852033f03a1891f1c852dadcfc400dd80cc8a2
ManaliParab12 <manaliparab10@gmail.com>
2020-10-28 14:41:46 +0530

    Created Getter, Setter and toString Method in Person Class

--- addressBook/Person.java
+++ addressBook/Person.java
@@ -16,4 +16,60 @@ public class Person {
 		this.phoneNumber = phoneNumber;
 		this.zip = zip;
 	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public long getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public int getZip() {
+		return zip;
+	}
+
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+
+	public String toString() {
+		return "Person [firstName=" + firstName + ", lastName=" + lastName + ", city=" + city + ", state=" + state
+				+ ", phoneNumber=" + phoneNumber + ", zip=" + zip + "]";
+	}
+	
+	
+
 }
