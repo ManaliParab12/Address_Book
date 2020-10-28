db50b7a8ee02e338a3708c5b6e06721cfbbd498f
ManaliParab12 <manaliparab10@gmail.com>
2020-10-28 14:53:48 +0530

    Created Address Book Interface with all the method declaration

--- addressBook/AddressBookInterface.java
+++ addressBook/AddressBookInterface.java
@@ -1,5 +1,14 @@
 package addressBook;
 
 public interface AddressBookInterface {
+	
+	public void addPerson();
+	public void editPerson();
+	public void deletePerson();
+	public void sortByName();
+	public void sortByZip();
+	public void searchPerson();
+	public void display();
+
 
 }
