defd3d4ff4fb43e8800700dafd6bedc3a15e5053
ManaliParab12 <manaliparab10@gmail.com>
2020-10-28 16:05:50 +0530

    Using switch case to perform operation of openAddressbook method Address Book Main class

--- addressBook/AddressBookMain.java
+++ addressBook/AddressBookMain.java
@@ -8,15 +8,15 @@ public class AddressBookMain {
 	public static void main(String [] args) {
 		
 		AddressBookManager addressbookmanager = new AddressBookManager();
-		
+		AddressBook addressbook = new AddressBook();
 		int answer=1;
 		while (answer == 1) {			
 			System.out.println("Select any option for Address Book Operation");
 			System.out.println("1. Create New AddressBook"
-					   		 + "\n2. open existing AddressBook"
-							 + "\n3. save AddressBook"
-			                 + "\n4. Save As Address Book"
-		                     + "\n5. Close Address Book");			
+							+ "\n2. open existing AddressBook"
+							+ "\n3. save AddressBook"
+							+ "\n4. Save As Address Book"
+							+ "\n5. Close Address Book");			
 			int choice =sc.nextInt();
 			
 			switch(choice) {
@@ -25,6 +25,49 @@ public class AddressBookMain {
 				break;
 			case 2 :
 				addressbookmanager.openAddressBook();
+				
+				int input=1;
+				while (input == 1) {					
+					System.out.println("Select any option From Menu ");
+					System.out.println("1. Add Person"
+									+ "\n2. Edit Person"
+									+ "\n3. Delete Person"
+									+ "\n4. Sort Person by Last Name"
+									+ "\n5. Sort Person by Zip Code"
+									+ "\n6. Search Person"
+									+ "\n7. Display"
+									+ "\n8. Quit");
+					int value = sc.nextInt();
+					
+					switch (value) {
+					case 1 :
+						addressbook.addPerson();
+						break;
+					case 2 :
+						addressbook.editPerson();
+						break;
+					case 3 :
+						addressbook.deletePerson();
+						break;
+					case 4 :
+						addressbook.sortByName();
+						break;
+					case 5 :
+						addressbook.sortByZip();
+						break;
+					case 6 :
+						addressbook.searchPerson();
+						break;
+					case 7 :
+						addressbook.display();
+						break;
+					case 8 :
+						input =0;
+						break;
+					default :
+						System.out.println("Incorrect Choice" + "\nEnter a number between 1 and 8");			
+				    }
+				 }
 				break;					
 			case 3 :
 				addressbookmanager.saveAddressBook();
