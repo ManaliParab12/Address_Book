bb9e104c6d7332e1c2e79b9a15c78348065d59be
ManaliParab12 <manaliparab10@gmail.com>
2020-10-28 15:04:58 +0530

    Created Address Book class with the Structure of all AddressBookInterface Method

---
+++ addressBook/AddressBook.java
@@ -0,0 +1,47 @@
+package addressBook;
+
+public class AddressBook implements AddressBookInterface  {
+
+	@Override
+	public void addPerson() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void editPerson() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deletePerson() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sortByName() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sortByZip() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void searchPerson() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void display() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
