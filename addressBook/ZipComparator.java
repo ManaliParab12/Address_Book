48888a721b6dbe78f868a64d97d3e41eb62b35ca
ManaliParab12 <manaliparab10@gmail.com>
2020-10-29 17:06:22 +0530

    ZipComparator class created with Constructor

---
+++ addressBook/ZipComparator.java
@@ -0,0 +1,12 @@
+package addressBook;
+
+import java.util.Comparator;
+
+public class ZipComparator implements Comparator<Person>{
+
+	public int compare(Person o1, Person o2) {
+
+		return 0;
+	}
+
+}
